3edb54dbd238418e355b55f58c8718a5d98e6007
sj9608 <sneak7581@gmail.com>
2021-08-03 15:45:36 +0900

    정수정렬(2750이랑 똑같음)

---
+++ BaekJoon/standard_io/2751/01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    int *arr = new int[N];
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N; i++)
+        cout << arr[i] << "\n";
+
+    return 0;
+}
